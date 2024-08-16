43963d97c09aedc11c798982f7c6cb73961ca22e
Marc F <marc.frenette@mail.utoronto.ca>
2024-08-15 23:06:06 -0400

    Quick change to the about me WIP text to include a link to the development build.

--- about.php
+++ about.php
@@ -12,5 +12,7 @@
 			<?php require_once("Resources/nav.php"); ?>
 		</header>
     </body>
-    This web page is currently under development.
+    <h1> Welcome to my "about me" page! </h1>
+	This page is currently being developed.
+	<a href="https://dev.marcf.ca/about">See progress on the development build here.</a>
 </html>
